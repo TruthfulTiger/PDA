4957ee551e2b3746e69b6b3ff0c6486eebc29872
Sam Phoenix <samgoacher79@gmail.com>
2018-12-28 18:15:03 +0000

    Update

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -5,10 +5,13 @@ class HomeController extends Controller {
 	public function index()
 	{
         $feed = "https://www.phoenixdragon.co.uk/blog/feed/";
+        $etsy = "https://www.etsy.com/shop/PhoenixDragonArcade/rss";
         $rss = Web::instance();
-		$this->f3->set('page_head','Updates');
-        $result[0] = $rss->rss($feed, 4);
+        $result[0] = $rss->rss($feed);
+        $etsyres[0] = $rss->rss($etsy);
         $this->f3->set('rss',$result[0]['feed']);
+        $this->f3->set('etsy',$etsyres[0]['feed']);
+        $this->f3->set('page_head','Updates');
 		$this->f3->set('page_head','Home');
 		$this->f3->set('view','/home.html');
 	}
